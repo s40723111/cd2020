efe55f6a8f151ac960092550cffab77b7168e4d1
s40723111 <40723111@gm.nfu.edu.tw>
2020-04-29 00:31:33 +0800

    4/29

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = '40723111'
+AUTHOR = 's40723111'
 SITENAME = '協同產品設計實習'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
